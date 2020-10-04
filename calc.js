f29fb4cd3a03cb677928e29ee13f64a045aa04f6
murilopanosso <murilopanosso@gmail.com>
2020-10-04 14:08:59 +0100

    initial

---
+++ calc.js
@@ -0,0 +1,18 @@
+const calc = require('@murilopanosso/basic-calc')
+
+const getResult = (expression = "") => {
+    console.log(expression.split("/").length)
+        if (expression.split("+").length > 1){
+            return  calc.add(parseFloat(expression.split("+")[0]),  parseFloat(expression.split("+")[1]))
+        } else if (expression.split("-").length > 1){
+            return  calc.subtraction(parseFloat(expression.split("-")[0]),  parseFloat(expression.split("-")[1]))
+        } else if (expression.split("/").length > 1) {
+            return  calc.divisiton(parseFloat(expression.split("/")[0]),  parseFloat(expression.split("/")[1]))
+        } else if (expression.split("*").length > 1) {
+            return  calc.multiplication(parseFloat(expression.split("*")[0]),  parseFloat(expression.split("*")[1]))
+        } else {
+            return "Expression not idenfied =("
+        }
+} 
+
+module.exports = getResult;
